a56077bcb21a72eddc1184e08e9c78c63b8f8288
Hubert Aschermann <aschehub@fit.cvut.cz>
2022-12-11 01:40:35 +0100

    UI: BooksController

--- src/main/java/haschman/library_client/web/UI/BooksController.java
+++ src/main/java/haschman/library_client/web/UI/BooksController.java
@@ -1,5 +1,7 @@
 package haschman.library_client.web.UI;
 
+import haschman.library_client.web.service.BookService;
+import jakarta.ws.rs.ClientErrorException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/Books")
 public class BooksController {
+    private final BookService bookService;
+
+    public BooksController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping
-    public String index(Model model) {
+    public String listAll(Model model) {
+        try {
+            model.addAttribute("books", bookService.readAll());
+        } catch (ClientErrorException e) {
+            model.addAttribute("error", true);
+            model.addAttribute("errormsg", e.getMessage());
+        }
+
         return "books";
     }
 }
