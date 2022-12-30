d994fcac16f2dc6c41493818bbe6027cc668cacc
Hubert Aschermann <aschehub@fit.cvut.cz>
2022-12-30 19:29:38 +0100

    templates: newAuthor

--- src/main/java/haschman/library_client/web/UI/controller/AuthorsController.java
+++ src/main/java/haschman/library_client/web/UI/controller/AuthorsController.java
@@ -1,11 +1,14 @@
 package haschman.library_client.web.UI.controller;
 
+import haschman.library_client.web.UI.model.AuthorModel;
 import haschman.library_client.web.domain.AuthorDTO;
 import haschman.library_client.web.service.AuthorService;
 import jakarta.ws.rs.ClientErrorException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Comparator;
@@ -33,5 +36,26 @@ public class AuthorsController {
             model.addAttribute("errorMsg", e.getMessage());
         }
         return "authors";
+    }
+
+    @GetMapping("/new")
+    public String create(Model model) {
+        model.addAttribute("authorModel", new AuthorModel());
+        return "newAuthor";
+    }
+
+    @PostMapping("/new")
+    public String createSubmit(@ModelAttribute AuthorModel authorModel, Model model) {
+        try {
+            AuthorDTO authorDTO = authorService.create(authorModel);
+            model.addAttribute("success", true);
+            model.addAttribute("successMessage", "Author successfully created with id: " + authorDTO.id);
+            model.addAttribute("authorModel", authorDTO);
+        } catch (Exception e) {
+            model.addAttribute("success", false);
+            model.addAttribute("errorMessage", e.getMessage());
+            model.addAttribute("authorModel", authorModel);
+        }
+        return "newAuthor";
     }
 }
---
+++ src/main/java/haschman/library_client/web/UI/model/AuthorModel.java
@@ -0,0 +1,6 @@
+package haschman.library_client.web.UI.model;
+
+import haschman.library_client.web.domain.AuthorDTO;
+
+public class AuthorModel extends AuthorDTO {
+}
