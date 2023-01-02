ecf9c4ebfb8dc7d6cdd09d2201f4fe250495165e
Hubert Aschermann <aschehub@fit.cvut.cz>
2023-01-02 16:18:07 +0100

    domain: LocationDTO

---
+++ src/main/java/haschman/library_client/web/domain/LocationDTO.java
@@ -0,0 +1,22 @@
+package haschman.library_client.web.domain;
+
+public class LocationDTO {
+    public int stand;
+    public int shelf;
+
+    public int getStand() {
+        return stand;
+    }
+
+    public void setStand(int stand) {
+        this.stand = stand;
+    }
+
+    public int getShelf() {
+        return shelf;
+    }
+
+    public void setShelf(int shelf) {
+        this.shelf = shelf;
+    }
+}
